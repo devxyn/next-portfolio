06212fb8f85e1a24db776d63ac12188bb90430cc
devxyn <johnlawrencehermo@gmail.com>
2023-12-27 18:49:54 +0800

    feat: HeroSection

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <h1 className="text-white">Portfolio</h1>
+    </section>
+  );
+};
+
+export default HeroSection;
