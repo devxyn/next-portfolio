b0741a61e71a212eda4f786d192e94d489cbaa97
devxyn <johnlawrencehermo@gmail.com>
2024-02-01 19:21:48 +0800

    fix: fixed padding width

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,9 @@ import ProjectSection from "./components/ProjectSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <main className="flex min-h-screen flex-col bg-[#121212] md:scroll-auto">
       <Navbar />
-      <div className="container mx-auto px-12 py-4 mt-16">
+      <div className="container mx-auto px-6 md:px-8 py-4 mt-16">
         <HeroSection />
         <AboutSection />
         <ProjectSection />
