3a86eb02054fc113ddddb4f1d7d0467ffd646b68
devxyn <johnlawrencehermo@gmail.com>
2024-02-01 22:09:37 +0800

    feat: added job description for experience section

--- src/app/utils/aboutSectionData.js
+++ src/app/utils/aboutSectionData.js
@@ -16,10 +16,21 @@ export const experienceData = [
     title: "Shopify Developer",
     company: "Nordic Quality",
     date: "Jul 2022 - Mar 2023",
+    description: [
+      "Building custom storefronts on Shopify using ReactJS, NextJS, React-based framework.",
+      "Modifying Shopify themes.",
+      "Adding apps to Shopify Store.",
+      "Enabling payment gateways.",
+    ],
   },
   {
     title: "Full-stack Web Developer Intern",
     company: "Uplift Code Camp",
     date: "Aug 2023 - Mar 2024",
+    description: [
+      "6 Month Full Stack Web Development Bootcamp.",
+      "Participating in code reviews and providing constructive feedback with other bootcamp students.",
+      "Created full stack apps using MERN Stack.",
+    ],
   },
 ];
